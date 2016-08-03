a6b41b727ba6deb50071d1071ad9d1e29afeb1cf
André Moabson <amsr@ic.ufal.br>
2016-08-03 11:45:44 -0300

    implementação das funções e instruções ADD e HALT

--- Interpreter.py
+++ Interpreter.py
@@ -1,11 +1,12 @@
 from enum import Enum
 
+
 class InstructionType(Enum):
     HALT = 1
     ADD = 2
 
-class Interpreter(object):
 
+class Interpreter(object):
     PC = None
     AC = 0
     instr = None
@@ -14,24 +15,64 @@ class Interpreter(object):
     data = None
     run_bit = True
     memory = []
+    instructions = {
+        1: InstructionType.ADD,
+        2: InstructionType.HALT
+    }
 
-    def interpret(self, memory, startingAddress):
-        self.memory = memory;
+    def interpret(self, memory, starting_address):
+        self.memory = memory
+        self.PC = starting_address
 
-        PC = startingAddress;
+        while self.run_bit:
+            self.instr = self.memory[self.PC]
+
+            print(self.instr)
+
+            self.instr_type = self.get_instr_type(self.instr)
+
+            if self.instr_type != InstructionType.HALT:
+                self.data_loc = self.find_data(self.instr, self.instr_type)
+                self.PC += 1
+                if self.data_loc >= 0:
+                    self.data = self.memory[self.data_loc]
+                    self.PC += 1
+
+            self.execute(self.instr_type, self.data)
 
     def get_instr_type(self, opcode):
-        print("")
+        return self.instructions.get(opcode)
 
-    def find_data(self, instr, instrType):
-        print("")
+    def find_data(self, instr, type):
+        for i in range(self.PC, len(memory)):
+            if self.get_instr_type(instr) == type:
+                return i + 1
 
-    def execute(self, instrType, data):
-        print("")
+        return -1
+
+    def execute(self, type, data):
+        if type == InstructionType.HALT:
+            self.run_bit = False
+        elif type == InstructionType.ADD:
+            self.AC += data
 
 
 interpreter = Interpreter()
 
-memory = ["ADD 1", "HALT"]
+memory = [1, 29, 1, 1, 2]
 
 interpreter.interpret(memory, 0)
+
+print(interpreter.AC)
+
+# interpreter.memory = memory;
+
+# instr_type = interpreter.get_instr_type(1)
+# data_loc = interpreter.find_data(1, instr_type)
+
+# print(interpreter.memory[data_loc])
+
+# for i in range(len(memory)):
+#   print(i)
+
+# interpreter.interpret(memory, 0)
