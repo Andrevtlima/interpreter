8d9a10bfabf19fe023879f4034194a5d3a42c974
André Moabson <amsr@ic.ufal.br>
2016-08-03 08:59:14 -0300

    estrutura inicial

--- Interpreter.py
+++ Interpreter.py
@@ -2,18 +2,36 @@ from enum import Enum
 
 class InstructionType(Enum):
     HALT = 1
-    ADD = 1
+    ADD = 2
 
 class Interpreter(object):
 
+    PC = None
+    AC = 0
+    instr = None
+    instr_type = None
+    data_loc = None
+    data = None
+    run_bit = True
     memory = []
 
-    def __init__(self):
-        print("Construtor")
+    def interpret(self, memory, startingAddress):
+        self.memory = memory;
 
-    def teste(self):
-        print("Hello")
+        PC = startingAddress;
+
+    def get_instr_type(self, opcode):
+        print("")
+
+    def find_data(self, instr, instrType):
+        print("")
+
+    def execute(self, instrType, data):
+        print("")
 
 
-teste = Interpreter()
-teste.teste()
+interpreter = Interpreter()
+
+memory = ["ADD 1", "HALT"]
+
+interpreter.interpret(memory, 0)
